ff594e1ad9ef9c176b5322e1a26f1d6ade8dda48
Kleber Henrique <yuk9.gabriel@gmail.com>
2023-03-11 15:50:47 -0300

    update

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,7 +1,17 @@
+import { MapPinLine } from "phosphor-react";
+import * as S from "./styles";
+
 export function Checkout() {
-    return(
-        <div>
-            
-        </div>
-    )
+  return (
+    <S.ContainerForm>
+      <div>
+        <S.Title>Complete seu pedido</S.Title>
+
+        <S.ContetentForm>
+            <h2><MapPinLine/>Endereço de Entrega</h2>
+            <span>Informe o endereço onde deseja receber seu pedido</span>
+        </S.ContetentForm>
+      </div>
+    </S.ContainerForm>
+  );
 }
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,1 +1,15 @@
 import styled from "styled-components";
+
+export const ContainerForm = styled.form`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 5rem;
+`
+
+export const Title = styled.h2`
+    font-family: 'Baloo 2', sans-serif;
+`
+
+export const ContetentForm = styled.div`
+background-color: ${props => props.theme.COLORS.BASE['BASE_CARD']};
+`
